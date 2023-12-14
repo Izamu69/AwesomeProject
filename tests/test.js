d377a12be40da881af163461ffda0f0bd183c233
StasKovalchuk <stasonkovalchuktron@gmail.com>
2023-12-14 12:33:14 +0200

    tests

--- tests/test.js
+++ tests/test.js
@@ -19,3 +19,27 @@ test('adding a task updates the tasks state', () => {
   expect(addedTask).toBeDefined();
 });
 
+test('editing a task updates it in the tasks state', () => {
+  // Render the component
+  const { getByText, getByPlaceholderText } = render(<App />);
+
+  // Mock some tasks
+  const originalTask = 'Original Task';
+  const updatedTask = 'Updated Task';
+
+  // Add a task to the state
+  fireEvent.changeText(getByPlaceholderText('Enter task'), originalTask);
+  fireEvent.press(getByText('Add Task'));
+
+  // Edit the task
+  const editButton = getByText('Edit');
+  fireEvent.press(editButton);
+
+  // Update the task
+  fireEvent.changeText(getByPlaceholderText('Enter task'), updatedTask);
+  fireEvent.press(getByText('Update Task'));
+
+  // Check if the task is updated in the tasks state
+  const updatedTaskElement = getByText(updatedTask);
+  expect(updatedTaskElement).toBeDefined();
+});
